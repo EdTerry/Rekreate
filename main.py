a8af840fbe1727ee67feeb7b85a92afa02287b62
Edward Terry <Edward Terry>
2017-09-30 16:59:01 -0400

    Made changes

--- main.py
+++ main.py
@@ -4,14 +4,10 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 
 def mse(imageA, imageB):
-	# the 'Mean Squared Error' between the two images is the
-	# sum of the squared difference between the two images;
 	# NOTE: the two images must have the same dimension
 	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
 	err /= float(imageA.shape[0] * imageA.shape[1])
 
-	# return the MSE, the lower the error, the more "similar"
-	# the two images are
 	return err
 
 while True:
@@ -19,11 +15,12 @@ while True:
     cv2.imshow('Video Output', cam)
 
     imgFile = cv2.imread('test.jpg')
-    resize_img = cv2.resize(imgFile, (480,640))
+    resize_img = cv2.resize(imgFile, (640,480))
     cv2.imshow('image', imgFile)
 
     #   Compare both images
-    print(mse(cam,imgFile))
+	# 	No longer using mse function, as it does not compare edges
+    #print(mse(cam,resize_img))
 
     k = cv2.waitKey(10) & 0xff
     if k == 27:
