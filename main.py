f839c659ab2aee40ca075385b7056cf6f2aba615
Edward Terry <Edward Terry>
2017-09-30 18:20:56 -0400

    Progress made with edge detection for video.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 def mse(imageA, imageB):
 	# NOTE: the two images must have the same dimension
@@ -11,16 +12,42 @@ def mse(imageA, imageB):
 	return err
 
 while True:
-    ret, cam = cap.read()
-    cv2.imshow('Video Output', cam)
+    ret, frame = cap.read()
+
+    # converting BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # define range of red color in HSV
+    lower_red = np.array([30,150,50])
+    upper_red = np.array([255,255,180])
+
+    # create a red HSV colour boundary and
+    # threshold HSV image
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    # Display an original image
+    cv2.imshow('Original',frame)
+
+    # finds edges in the input image image and
+    # marks them in the output map edges
+    cam_edges = cv2.Canny(frame,100,200)
+
+    # Display edges in a frame
+    cv2.imshow('Edges',cam_edges)
 
     imgFile = cv2.imread('test.jpg')
-    resize_img = cv2.resize(imgFile, (640,480))
-    cv2.imshow('image', imgFile)
+    imgFile_edges = cv2.Canny(imgFile,640,480)
+
+    resize_img = cv2.resize(imgFile_edges, (640,480))
+
+    cv2.imshow('image', resize_img)
 
     #   Compare both images
 	# 	No longer using mse function, as it does not compare edges
-    #print(mse(cam,resize_img))
+    print(mse(cam_edges,resize_img))
 
     k = cv2.waitKey(10) & 0xff
     if k == 27:
